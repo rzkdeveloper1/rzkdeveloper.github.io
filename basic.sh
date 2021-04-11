cd3ad6b091de7827546c34a3d9e3e9acce5f0f2d
MefelemS <72390888+MefelemS@users.noreply.github.com>
2021-04-11 14:14:23 +0700

    Add files via upload

---
+++ basic.sh
@@ -0,0 +1,17 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo $yellow"Masukin uuid Dan Password Yang Bener!!"
+echo
+read -p "uuid : " id
+
+if [ $id = 508615531 ] || [ $id = 508615531 ]
+then
+sh pasw.sh
+fi
+
